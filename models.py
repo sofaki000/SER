f82822294b2a0a82b983575f5ec3d9c7c60d03b1
sofaki000 <58658186+sofaki000@users.noreply.github.com>
2022-11-23 23:34:06 +0200

    one file

---
+++ models.py
@@ -0,0 +1,20 @@
+import  keras.optimizers as optim
+from keras import Sequential
+from keras.layers import Dense
+from keras.layers import BatchNormalization
+
+def get_model(num_of_output_classes,input_dim, lr=0.01):
+	model = Sequential()
+	model.add(Dense(15, input_dim=input_dim, activation='relu'))
+	model.add(BatchNormalization())
+	model.add(Dense(num_of_output_classes, activation='softmax'))
+	optimizer = optim.Adam(learning_rate=lr)
+	model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+	return model
+
+def get_trained_model(trainX, trainy, n_epochs,num_of_output_classes):
+	model = get_model(num_of_output_classes=num_of_output_classes,input_dim=40)
+	# train model
+	model.fit(trainX, trainy, epochs=n_epochs, verbose=0)
+	return model
