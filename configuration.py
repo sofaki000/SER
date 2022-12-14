f110dc8a3edb4dc937a5c7ea01fe97e77c22a01a
maroulou <stathmarw@gmail.com>
2022-12-14 21:24:37 +0200

    τα διαγράμμαα πρόσθεσα για πριν και μετά το augmentation και τα σπεκτογραφήματα .Επίσης στο onfiguration άλλαξα το path

--- configuration.py
+++ configuration.py
@@ -1,4 +1,5 @@
 
 n_epochs = 400
 learning_rate=0.01
-data_path = "C:\\Users\\Lenovo\\Desktop\\ser\\SER\\data\\test"
+#data_path = "C:\\Users\\Lenovo\\Desktop\\ser\\SER\\data\\test"
+data_path = "C:\\Users\\user\\Documents\\AVT_SER__Κώδικας_Χρύσας\\SER_xrysas\\data\\test"
